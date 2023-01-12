72bc98fdd2c6325029ceff405cefab953afbb0e3
Guiomar Cameras <ada_hack243@hackademy.mx>
2023-01-12 12:10:05 -0600

    variables con sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 const { src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 function css(done) {
- 
-    src('src/scss/app.scss') //Identificar el archivo SASS
+    src('src/scss/**/*.scss') //Identificar el archivo SASS
         .pipe( sass()) //Compilar
         .pipe(dest("build/css")) //Almacenarla en el disco duro
 
@@ -10,7 +9,7 @@ function css(done) {
 }
 
 function dev(done){
-    watch('src/scss/app.scss', css)
+    watch('src/scss/**/*.scss', css)
     done();
 }
 exports.css = css;
