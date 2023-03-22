14d4aebcee528ef0695f0a90079f23e811bdfa34
Guiomar Cameras <ada_hack243@hackademy.mx>
2023-03-22 12:23:02 -0600

    Aligerar imágenes con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
 
 //Imágenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -14,6 +16,16 @@ function css(done) {
         .pipe(dest("build/css")) //Almacenarla en el disco duro
 
     done();//Calback que avisa a gulp cuando llegamos al final
+}
+
+function imagenes(done) {
+    const opciones = {
+        optimizationLevel:3
+    }
+    src('src/img/**/*.{png,jpg}')
+        .pipe( cache(imagemin(opciones)))
+        .pipe( dest('build/img'))
+    done();
 }
 
 function versionWebp(done) {
@@ -32,6 +44,7 @@ function dev(done){
     done();
 }
 exports.css = css;
+exports.imagenes = imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp, dev);
+exports.dev = parallel(imagenes, versionWebp, dev);
 
