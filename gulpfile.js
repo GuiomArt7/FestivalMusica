555f24ac8be640ed49e4a665147d8222d5580bf8
Guiomar Cameras <ada_hack243@hackademy.mx>
2023-03-22 11:40:26 -0600

    Gulp para imágenes Webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch, parallel } = require("gulp");
+
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require('gulp-plumber');
+
+//Imágenes
+const webp = require('gulp-webp');
+
 function css(done) {
     src('src/scss/**/*.scss') //Identificar el archivo SASS
         .pipe(plumber())
         .pipe( sass()) //Compilar
-        
         .pipe(dest("build/css")) //Almacenarla en el disco duro
 
     done();//Calback que avisa a gulp cuando llegamos al final
+}
+
+function versionWebp(done) {
+
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{png,jpg}')
+        .pipe( webp(opciones) )
+        .pipe( dest("build/img"))
+    done();
 }
 
 function dev(done){
@@ -16,4 +32,6 @@ function dev(done){
     done();
 }
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
+
