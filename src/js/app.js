34b22c789783b42190291651aca846ae864016ed
Guiomar Cameras <ada_hack243@hackademy.mx>
2023-04-26 17:11:53 -0600

    Añadir un botón para cerrar la imagen

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,29 @@ function mostrarImagen(id) {
     <img loading="lazy" width="200" height="300" src="build/img/grande/${id}.jpg" alt="Imagen Galería">
     `;
 
+    //Crea el Overlay con la imagen
     const overlay = document.createElement('DIV');
     overlay.appendChild(imagen);
     overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
 
+    //Botón para cerrar el modal
+    const cerrarModal = document.createElement('P');
+    cerrarModal.textContent = 'X';
+    cerrarModal.classList.add('btn-cerrar');
+    cerrarModal.onclick = function(){
+        const body = document.querySelector('body');
+        body.classList.remove('fijar-body');
+        overlay.remove();
+    }
+    overlay.appendChild(cerrarModal);
+    
+    //Añadirlo al HTML
     const body = document.querySelector('body');
     body.appendChild(overlay);
+    body.classList.add('fijar-body');
 }
