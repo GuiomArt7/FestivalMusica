86d2c8bfb796bad22e3b3ef31a38d45437d8f12d
Guiomar Cameras <ada_hack243@hackademy.mx>
2023-04-28 14:11:23 -0600

    Smooth scroll y fijar barra al hacer scroll

--- build/js/app.js
+++ build/js/app.js
@@ -3,7 +3,41 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 function iniciarApp(){
+    navegacionFija();
     crearGaleria();
+    scrollNav();
+}
+
+function navegacionFija() {
+    const barra = document.querySelector('.header');
+    const sobreFestival = document.querySelector('.sobre-festival');
+    const body = document.querySelector('body');
+
+    window.addEventListener('scroll', function() {
+       
+        if( sobreFestival.getBoundingClientRect().top < 0){
+            barra.classList.add('fijo');
+            body.classList.add('body-scroll');
+        } else {
+            barra.classList.remove('fijo');
+            body.classList.remove('body-scroll');
+        }
+    })
+}
+function scrollNav(){
+    const enlaces = document.querySelectorAll('.navegacion-principal a');
+                                               
+
+    enlaces.forEach( enlace => {
+        enlace.addEventListener('click', function(e){
+            e.preventDefault();//Prevenir reload por default
+
+            const seccionScroll = e.target.attributes.href.value;
+            const seccion = document.querySelector(seccionScroll);
+            seccion.scrollIntoView({ behavior: 'smooth'}); 
+        });
+    });
+
 }
 
 function crearGaleria(){
